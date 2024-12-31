5d84aea4869ad89854bf85b81e446182252981b8
Haderacher <liu235991@gmail.com>
2024-12-31 13:48:13 +0800

    1

--- app/yanshengpin/admin.py
+++ app/yanshengpin/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib.admin import AdminSite
 from .models import Author
 from .models import OriginWork
 from .models import DerivativeDevelopeDepartment
@@ -7,13 +7,16 @@ from .models import Product
 from .models import Authorize
 from .models import ProductionCommission
 
+class MyAdminSite(AdminSite):
+    site_header = "IP公司管理"
 
+admin_site = MyAdminSite(name="myadmin")
 # Register your models here.
 
-admin.site.register(Author)
-admin.site.register(OriginWork)
-admin.site.register(DerivativeDevelopeDepartment)
-admin.site.register(DevelopTeam)
-admin.site.register(Product)
-admin.site.register(Authorize)
-admin.site.register(ProductionCommission)
+admin_site.register(Author)
+admin_site.register(OriginWork)
+admin_site.register(DerivativeDevelopeDepartment)
+admin_site.register(DevelopTeam)
+admin_site.register(Product)
+admin_site.register(Authorize)
+admin_site.register(ProductionCommission)
